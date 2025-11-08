6e0b6c9e058ac1e5d2a0eba54e89bb216bafad04
Ruben0304 <hernandzruben9@gmail.com>
2025-11-08 10:29:37 -0500

    Initial Commit

---
+++ Financia/FinanciaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanciaApp.swift
+//  Financia
+//
+//  Created by Ruben on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FinanciaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
